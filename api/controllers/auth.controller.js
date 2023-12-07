e10aaa727a4aa596ba7c4881238e7454c227acdc
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2023-12-08 02:31:44 +0530

    create a error function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,23 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-export const signup = async (req,res) =>{
+import { errorHandler } from '../utils/error.js';
+
+
+export const signup = async (req,res,next) =>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);       //hash sync(means it's awiat) 10 is salt 
     const newUser = new User({username,email,password:hashedPassword});
     try{
         await newUser.save();
         return res.status(201).json("User created succesfully");    
-    }catch(err){
-        return res.status(500).json(err.message);
+    }
+    // Intially 
+    // catch(err){
+    //     return res.status(500).json(err.message);
+    // }
+    //After adding middleware
+    catch(err){
+        next(err);
+        // next(errorHandler(500,'error from function')); // we created  this err used when password mistach by using upper line it's not error  
     }
 }

fb67668d2dd387c5969de5f0d5ffa88eca5c2bae
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2023-12-08 02:29:08 +0530

    create middleware function to handle possible err

--- api/index.js
+++ api/index.js
@@ -34,3 +34,14 @@ app.get('/test',(req,res)=>{
 
 app.use('/api/user',test);
 app.use('/api/auth',authRouter);
+
+//error middleware
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode : statusCode,
+        message,
+    });
+})

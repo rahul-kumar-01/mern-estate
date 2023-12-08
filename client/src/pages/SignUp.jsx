c1cfadef188664aaf973fb381d533ad46fda0df0
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2023-12-08 22:12:45 +0530

    complete signup functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom'
 
-export default function SignUp() {
+export default function SignUp() { 
+  const navigate = useNavigate();
+  const [formData,setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const handleChange = (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    })
+  };
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // const res = await fetch('/api/auth/signup' ,formData);  we have to stringfy the formdata it's not secure 
+    // if error come  use try and catch to fix it in catch setEroor(err.message);
+    const res = await fetch('/api/auth/signup',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(formData)
+    });
+    const data = await res.json();
+    if(data.success === false) {
+      setError(data.message);
+      setLoading(false);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
+  }
+  
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className=' flex flex-col gap-4'>
-        <input className="border p-3 rounded-lg id='username'" type="text" placeholder='username'/>
-        <input className="border p-3 rounded-lg id='email'" type="text" placeholder='email'/>
-        <input className="border p-3 rounded-lg id='password'" type="text" placeholder='password'/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      <form className=' flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input className="border p-3 rounded-lg" id='username' type="text" placeholder='username' onChange={handleChange}/>
+        <input className="border p-3 rounded-lg" id='email' type="text" placeholder='email' onChange={handleChange}/>
+        <input className="border p-3 rounded-lg" id='password' type="text" placeholder='password' onChange={handleChange}/>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign up' } 
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
@@ -17,6 +56,7 @@ export default function SignUp() {
             <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/  
 export default defineConfig({
+  server:{
+    proxy:{
+      // '/api': 'http://localhost:3000'   can't use this becuase it's not secure each time you /api add http://localhost:3000 at beginning
+      '/api':{
+        target: 'http://localhost:3000',
+        secure:false,
+      },
+    },
+  },
   plugins: [react()],
 })

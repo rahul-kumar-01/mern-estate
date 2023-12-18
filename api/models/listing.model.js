ac467a78e07a2a538b6fb75432bde008cd75a792
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2023-12-19 00:54:05 +0530

    listing page functionality

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -45,7 +45,7 @@ const listingSchema = new mongoose.Schema({
         type:Boolean,
         required: true,
     },
-    imageUrls : {
+    imagesUrls : {
         type: Array,
         required : true
     },

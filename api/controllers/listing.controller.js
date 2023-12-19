8fda7ac19e414d5782687560a52e0cc3445b6354
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2023-12-19 20:31:17 +0530

    complete delete user listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,11 +1,35 @@
 import Listing from "../models/listing.model.js";
+import {errorHandler} from '../utils/error.js'
 
 
 export const createListing = async(req,res,next) => {
     try{
+        console.log('asdfasf');
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
     }catch(err){
         next(err);
     }
+}
+
+export const deleteListing = async (req,res,next) => {
+    console.log("asfdj');");
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing) {
+        return next(errorHandler(401,'Listing not found'));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'You can only delete your own listing'));
+    }
+
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        return res.status(200).json('Listing has been deleted');
+    }catch(error){
+        next(err);
+    }
+
+
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -102,7 +102,6 @@ export default function Profile() {
   const handleSignOut = async () => {
     try{
       dispatch(signOutStart());
-      console.log("asdf");
       const data = await fetch('/api/auth/signout');
       if(data.success === false){
         dispatch(signOutFailure(data.message));
@@ -127,6 +126,24 @@ export default function Profile() {
       setUserListing(data);
     }catch(error){
       setShowListingError(error); 
+    }
+  }
+
+  const handleListingDelete = async (listingId) => {
+    try{
+      const res = await fetch(`/api/listing/delete/${listingId}` ,{
+        method : 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+
+      setUserListing((prev) => prev.filter((listing) => listing._id !== listingId))
+
+    }catch(error){
+      console.log(error.message);
     }
   }
 
@@ -214,15 +231,11 @@ export default function Profile() {
             </Link>
 
             <div className='flex flex-col items-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
             </div>
             <div>
-              <button>
-
-              </button>
-              <button>
-              </button>
+              
             </div>
 
           </div>

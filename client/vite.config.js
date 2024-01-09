9d08c1ed8f91ff7522b6f1a8e88d13bedb4b8817
Rahul Kumar Thakur <rahul.thakur.coder.01@gmail.com>
2024-01-09 11:25:37 +0530

    j

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vitejs.dev/config/  
 export default defineConfig({
+  root: path.join(__dirname, 'src'),
   server:{
     proxy:{
       // '/api': 'http://localhost:3000'   can't use this becuase it's not secure each time you /api add http://localhost:3000 at beginning
